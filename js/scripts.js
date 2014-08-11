75d4f23adf3c864acbc9527e4384250614897b45
dimitrie <Bender@Benders-MacBook-Pro.local>
2014-08-11 09:51:37 +0200

    Added Recipe Set Menu

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ Number.prototype.map = function ( in_min , in_max , out_min , out_max ) {
 
 function randomIngredient()
 {
-	var words = "carrot cake polenta asparagus brocoli musli lime steam lamb pork tulips deodorant".split(' ');
+	var words = "carrot-cake polenta asparagus-brocoli musli lime leek-salad steam lamb pork tulips deodorant mashed-potatoes".split(' ');
 	return words[Math.floor(Math.random() * words.length)];
 }
 
@@ -40,40 +40,28 @@ function getRandomColor() {
 */
 
 function setElementsHeight(){
-	var totalHeight = $("body").height();
-	var unit = totalHeight/9;
 
-	var elemNo = $(".dice").length;
-	var unitD = (totalHeight-unit)/elemNo;
-	
-	$(".dice").each(function(i) { 
-		$(this).height(unitD); 
-		$(this).css("top", i*unitD); 
-	});
+	var elemno = $(".dice").length;
 
 	$(".dice").each(function(){
 		$(this).css("background-color", getRandomColor());
+		$(this).css("height", 100/elemno + "%");
 	});
 
-	$(".dice .ingredient").css("line-height", unitD+"px");
-	$(".dice .phrase").css("line-height", unitD+"px");
-	$(".dice .funny-text").css("line-height", unitD+"px");
-
-	$(".menu").height(unit*1);
-	$(".menu").css("line-height", unit*1+"px");
-	//$(".vegToggle").height(unit*1);
-	$(".vegToggle").css("line-height", unit*0.7+"px");
-
-	$(".menu").velocity({boxShadowBlur:10}, 0);
-	//$(".vegToggle").velocity({boxShadowBlur:10}, 0);
-
-	$(".svg-handler").each(function(index){
-		
-
-
-		var skewfactor = index.map(0,3, 10, 15) * (index%2==0 ? 1 : -1);
-		//$(this).velocity({rotateZ: 45}, 1);
+	$(".title").each(function(){
+		$(this).css("background-color", getRandomColor());
 	});
+
+	$(".dice .ingredient").css("line-height", $(".dice").height()*0.7+"px");
+	$(".dice .phrase").css("line-height", $(".dice").height()*0.3+"px");
+	
+	$(".menu").css("line-height", $("#menu-holder").height()+"px");
+	
+	$(".title").css("line-height", $(".title").height()+"px");
+
+
+	$("#menu-holder").velocity({boxShadowBlur:20}, 0);
+	
 }
 
 
@@ -82,14 +70,7 @@ var vegan = false;
 
 function changeRegime()
 {
-	$(".vegToggle").hammer().on("drag", function(e){
-		e.gesture.preventDefault();
-		
-		$(this).velocity({right: "-=" + e.gesture.deltaX.map(0, $("body").width(), 0, 50) + "%"},1)
-		//$(this).css("left", ($("body").width()*0.7 + e.gesture.deltaX) + "px");
-		$(this).velocity({opacity: Math.abs(e.gesture.deltaX).map(0,$("body").width(),1,0)}, 1);
 
-	});
 }
 
 function makeMenu()
@@ -110,35 +91,68 @@ $(document).ready(function () {
 
 	var dragInProcess = false;
 	var dragUpInProcess = false;
-	var collapsedState = false;
+	
+	var menuopen = false;
 
-	var sidebarOpen = false;
-	var sidebardDragLeft = false;
-	var sidebardDragRight = false;
-	var sideBarScale = 0;
+	var prevMargin = 0;
 
-	//
-	// VEGETARIAN TOGGLE
-	// no interaction conflicts for now
-	//
+	$(".title").css("height", "25%");
+	$(".title").css("line-height", $(".title").height() + "px");
 
+	$("#menu-holder").hammer().on('tap', function(e){
+		console.log("closing or opening something else.");
+		menuopen = !menuopen;
+
+		if(menuopen) {
+
+			var top = 0;
+		
+			$(this).velocity({top: top+"%"}, {duration: 400});
+
+			$(".inner-holder").velocity({marginTop: "0%"}, {duration: 1});
+			//$(".active").velocity("scroll", { duration: 1, container: $(".inner-holder"), offset: -100 });
+
+		}
+		else {
+			var top = 90;
+			$(this).velocity({top: top+"%"}, {duration: 350, complete: function(){
+
+				var absH = $("body").height();
+				var unitH = absH*0.1;
+				var activeIndex = 0;
+				var activeH = 0;
+
+
+				$(".title").each(function(index){ if($(this).hasClass("active")) { activeIndex = index; activeH = $(this).position().top; return; }});
+
+				//prevMargin = -unitH*activeIndex;
+
+				$(this).find(".inner-holder").velocity({marginTop: -activeH - $(".title").height()/3+"px"}, {duration: 1200, easing: [600,20]});
+
+			}});
+
+		}
+		
+	});
+
+
+	$(".title").hammer().on("tap", function(){
+		if(!menuopen) return;
+		$(".title").removeClass("active");
+		$(this).addClass("active");
+		$(this).bringToTop();	
+	});
 
 	//
 	// PULL TO REFRESH
 	// priority 0 in the event queue
 	//
 	$("body").hammer().on('hold', function (e) {
-		console.log("what is wrong?");
-		if(collapsedState) { 
-		 		$(".dc").velocity("fadeIn", {duration: 100, complete: function() {collapsedState = false;}});
-		 		$(".mc").velocity("fadeOut", "fast");
-		 	} 	
+	
 	});
 
-	$(".dice").hammer().on('dragdown', function(e){
+	$("#dice-holder").hammer().on('dragdown', function(e){
 			
-			if(collapsedState) return;
-
 			e.gesture.preventDefault(true);
 		 	dragInProcess = true;
 		 	
@@ -155,24 +169,32 @@ $(document).ready(function () {
 		 				}, index*40);
 		 		});
 				
+				$("body").velocity({backgroundColor: "#fff"}, 600);
 		 	} // end big if
 
 
 		}).on("dragend", function(e){
 			if(!dragInProcess) return; 
-			if(dragUpInProcess) return;
-			if(false) return;
 
 			console.log("released from drag");
 			kickOff = true;
+
+			$("body").velocity("stop", true);
+			$("body").velocity({backgroundColor: "#d7d7d7"}, 1);
 			
 			$(".dice").velocity("stop", true);//.velocity("reverse");
 			$(".dice").velocity({scale: 1, translateX:0, translateY:0}, {easing: [400,20], complete: function() {dragInProcess = false;}});
 			$(".dice").each(function(){ 
 				var color = getRandomColor();
 				$(this).css("background-color", color); 
-				$(this).find(".ingredient").css("background-color", color);
-				$(this).find(".ingredient").find(".ingredient-text").html(randomIngredient());
+				//$(this).find(".swiper").css("background-color", color);
+				$(this).find(".swiper").find(".ingredient-text").html(randomIngredient());
+
+				var noChars = $(this).find(".swiper").find(".ingredient-text").html().length;
+				var width = noChars * 19;
+
+				$(this).find(".svg-handler").css("width", width + "px");
+				$(this).find(".svg-handler").css("left", ($(this).width() - width)/2 + "px");
 			});
 	});
 
@@ -186,8 +208,7 @@ $(document).ready(function () {
 	var dragleftInProcess = false;
 	var focused = false;
 
-	$(".dice").hammer().on("touch", function(e) {
-		if(collapsedState) return;
+	$(".swipeable").hammer().on("touch", function(e) {
 		e.gesture.preventDefault(true);
 
 		var that = $(this);
@@ -207,47 +228,40 @@ $(document).ready(function () {
 	var refreshElement = false;
 	var rightDrag = false;
 
-	$(".dice").hammer().on("dragleft", function(e){
+	$(".swipeable").hammer().on("dragleft", function(e){
 		if(dragInProcess) return;
 		if(dragUpInProcess) return;
-		if(collapsedState) return;
 
 		e.gesture.preventDefault();
 		
 		if(!focused) { console.log("aha!"); return; }
 
-		$(this).find(".ingredient").css("left", ($("body").width()*0.4 + e.gesture.deltaX) + "px");
-		$(this).find(".ingredient").velocity({opacity: e.gesture.deltaX.map(0,-190,1,0)}, 1);
-		//$(this).find(".arrow").velocity({opacity: e.gesture.deltaX.map(0,-190,1,0)}, 1);
+		$(this).find(".swiper").css("left", (0 + e.gesture.deltaX) + "px");
+		$(this).find(".swiper").velocity({opacity: e.gesture.deltaX.map(0,-100,1,0)}, 1);
 
 		if(e.gesture.deltaX < -100)	refreshElement = true; 
 	});
 
-	$(".dice").hammer().on("dragright", function(e){
+	$(".swipeable").hammer().on("dragright", function(e){
 		if(dragInProcess) return;
 		if(dragUpInProcess) return;
-		if(collapsedState) return;
-
+		
 		e.gesture.preventDefault();
 		
 		if(!focused) { console.log("aha!"); return; }
 
 		rightDrag = true;
 
-		$(this).find(".ingredient").css("left", ($("body").width()*0.4 + e.gesture.deltaX) + "px");
-		$(this).find(".ingredient").velocity({opacity: e.gesture.deltaX.map(0,100,1,0)}, 1);
-		//$(this).find(".arrow").velocity({opacity: e.gesture.deltaX.map(0,-190,1,0)}, 1);
+		$(this).find(".swiper").css("left", (0 + e.gesture.deltaX) + "px");
+		$(this).find(".swiper").velocity({opacity: e.gesture.deltaX.map(0,100,1,0)}, 1);
 
 		if(e.gesture.deltaX > 100)	refreshElement = true; 
 	});
 
-	$(".dice").hammer().on("release", function(e){
+	$(".swipeable").hammer().on("release", function(e){
 				
 				if(dragInProcess) return;
 				if(dragUpInProcess) return;
-				if(collapsedState) return;
-
-				if(false) return;
 
 				if(touchInProcess) {
 					if(!holdAnim) $(this).velocity("stop", true);
@@ -256,29 +270,35 @@ $(document).ready(function () {
 				if(holdAnim) { $(this).velocity({boxShadowBlur: 0, scale:1}, {duration:300, complete: function() {touchInProcess = false;}});}
 
 				if(refreshElement){
-					console.log("refreshingElement");
 					refreshElement = false;
-					$(this).find(".ingredient").css("opacity", "0");
-					$(this).find(".ingredient").css("left", "40%");
+					$(this).find(".swiper").css("opacity", "0");
+					$(this).find(".swiper").css("left", "0%");
 					
 					var color = getRandomColor();
 					console.log(color);
 
-					$(this).find(".ingredient").css("background-color", color); 
+					//$(this).find(".swiper").css("background-color", color); 
 					//$(this).css("backgroundColor",color);
 					$(this).velocity({backgroundColor: color}, {duration: 250});
 
-					$(this).find(".ingredient").find(".ingredient-text").html(randomIngredient());
+					$(this).find(".swiper").find(".ingredient-text").html(randomIngredient());
 					
-					if(rightDrag) { $(this).find(".ingredient").velocity("transition.bounceLeftIn", 600); rightDrag = false; }
-					else $(this).find(".ingredient").velocity("transition.bounceRightIn", 600);
+					if(rightDrag) { $(this).find(".swiper").velocity("transition.bounceLeftIn", 600); rightDrag = false; }
+					else $(this).find(".swiper").velocity("transition.bounceRightIn", 600);
 				}
 				else{
-					if(rightDrag) { $(this).find(".ingredient").velocity({left:"40%", opacity:1}, 200); rightDrag = false; } 
-					else $(this).find(".ingredient").velocity({left:"40%", opacity:1}, 200);
+					if(rightDrag) { $(this).find(".swiper").velocity({left:"0%", opacity:1}, 200); rightDrag = false; } 
+					else $(this).find(".swiper").velocity({left:"0%", opacity:1}, 200);
 				}
 
-	
+
+				if(!$(this).hasClass("dice")) return;
+				var noChars = $(this).find(".swiper").find(".ingredient-text").html().length;
+				var width = noChars * 19;
+
+				$(this).find(".svg-handler").css("width", width + "px");
+				$(this).find(".svg-handler").css("left", ($(this).width() - width)/2 + "px");
+
 				//$(this).find(".arrow").velocity("transition.bounceLeftIn", 750);
 				
 	});
